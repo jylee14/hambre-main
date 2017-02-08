2f0be8e81852068c78f35bdc9351ad545ceec59f
Jonathankwchoi <jkc012@ucsd.edu>
2017-02-08 13:21:26 -0800

    Testing push from Jonathan

---
+++ app/src/main/java/com/example/jun/hambre_main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jun.hambre_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//Testing push from jonathan
