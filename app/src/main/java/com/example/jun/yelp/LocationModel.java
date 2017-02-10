fcd305d50963ee6f8307d061668797a38ac73775
Jun Lee <jyl150@ucsd.edu>
2017-02-10 15:27:22 -0800

    fixing coordinates.

--- app/src/main/java/com/example/jun/yelp/LocationModel.java
+++ app/src/main/java/com/example/jun/yelp/LocationModel.java
@@ -11,8 +11,6 @@ public class LocationModel {
     private String state;
     private String address1;
     private String zip_code;
-    private double latitude;
-    private double longitude;
 
     public String getCity(){
         return city;
@@ -42,18 +40,10 @@ public class LocationModel {
         return zip_code;
     }
 
-    public double getLatitude() { return latitude; }
-
-    public double getLongitude() { return longitude; }
-
     public String getAddress(){
         String mainAddress = address1 + " ";
         mainAddress += (address2 == null ? "" : address2) + " ";
         return mainAddress + city + " " + state + " " + zip_code;
-    }
-
-    public String getCoordinates(){
-        return latitude + "," + longitude;
     }
 
     public String toString(){
