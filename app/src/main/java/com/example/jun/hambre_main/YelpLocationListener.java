e675ee9a4152e0e591ad831033999d7608a70058
Paulos Liu <pll004@ucsd.edu>
2017-02-10 14:36:23 -0800

    swag it's brokent

---
+++ app/src/main/java/com/example/jun/hambre_main/YelpLocationListener.java
@@ -0,0 +1,31 @@
+package com.example.jun.hambre_main;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by paulosliu on 2/10/17.
+ */
+
+public class YelpLocationListener implements android.location.LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
