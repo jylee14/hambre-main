116645a84488aa72819c28519b905c250f6d11a8
Paulos Liu <pll004@ucsd.edu>
2017-02-10 16:35:48 -0800

    Got GPS to initially work.

--- app/src/main/java/com/example/jun/hambre_main/yelpStub.java
+++ app/src/main/java/com/example/jun/hambre_main/yelpStub.java
@@ -3,14 +3,17 @@ package com.example.jun.hambre_main;
 import com.google.gson.Gson;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -43,43 +46,46 @@ public class yelpStub extends AppCompatActivity {
     private TextView fourth;
 
     private BusinessModel business1;
+    private LocationManager locationManager;
+    private String provider;
+    private Activity myself;
 
     private int LOCATION_ACCESS_VALUE = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        myself = this;
         super.onCreate(savedInstanceState);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        // gps
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // get permissions
-        final YelpLocationListener locationListener = new YelpLocationListener();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
 
         setContentView(R.layout.yelpstub);
 
-        cuisine = (EditText)findViewById(R.id.culture);
-        show = (Button)findViewById(R.id.show);
-        first = (TextView)findViewById(R.id.first);
-        second = (TextView)findViewById(R.id.second);
-        third = (TextView)findViewById(R.id.third);
-        fourth = (TextView)findViewById(R.id.fourth);
+        cuisine = (EditText) findViewById(R.id.culture);
+        show = (Button) findViewById(R.id.show);
+        first = (TextView) findViewById(R.id.first);
+        second = (TextView) findViewById(R.id.second);
+        third = (TextView) findViewById(R.id.third);
+        fourth = (TextView) findViewById(R.id.fourth);
 
-        show.setOnClickListener(new View.OnClickListener(){
+        show.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 String culture = cuisine.getText().toString();
-                if(culture == "")
+                if (culture.equals(""))
                     Toast.makeText(yelpStub.this, "enter cuisine culture", Toast.LENGTH_SHORT).show();
                 else {
                     YelpApi api = new YelpApi();
 
-                    locationListener.onLocationChanged(location);
-
+                    if (ActivityCompat.checkSelfPermission(myself, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(myself, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        GPS_Enabled();
+                    }
+                    Location location = locationManager.getLastKnownLocation(provider);
                     // build params
                     HashMap<String, String> params = new HashMap<>();
                     params.put("latitude", String.valueOf(location.getLatitude()));
@@ -89,7 +95,7 @@ public class yelpStub extends AppCompatActivity {
                     params.put("radius_filter", "40000");
 
                     BusinessResponseModel businessResponse = api.businessSearch(params);
-                    try{
+                    try {
                         business1 = (businessResponse.businesses())[0];
                         first.setText(business1.name());
                         BusinessModel business2 = (businessResponse.businesses())[1];
@@ -98,7 +104,7 @@ public class yelpStub extends AppCompatActivity {
                         third.setText(business3.name());
                         BusinessModel business4 = (businessResponse.businesses())[3];
                         fourth.setText(business4.name());
-                    }catch(Exception e){
+                    } catch (Exception e) {
                         first.setText("something went wrong");
                         e.printStackTrace();
                     }
@@ -106,9 +112,9 @@ public class yelpStub extends AppCompatActivity {
             }
         });
 
-        first.setOnClickListener(new View.OnClickListener(){
+        first.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 String coordinates = business1.coordinates().toString();
                 String label = business1.name();
                 String uriBegin = "geo:" + coordinates;
@@ -120,5 +126,19 @@ public class yelpStub extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void GPS_Enabled() {
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to
+        // go to the settings
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
     }
 }
