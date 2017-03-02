1fe0f799647b5158b986a69afe72885078e4204e
Jun Lee <jy45lee@gmail.com>
2017-03-02 13:07:10 -0800

    Update DBSetPreferencesModel.java

--- app/src/main/java/com/irs/server/DBSetPreferencesModel.java
+++ app/src/main/java/com/irs/server/DBSetPreferencesModel.java
@@ -1,9 +1,5 @@
 package com.irs.server;
 
-/**
- * Created by bryle on 3/1/2017.
- */
-
 public class DBSetPreferencesModel {
     private String error;
     private String uid;
@@ -17,6 +13,7 @@ public class DBSetPreferencesModel {
     public String error(){return error;}
     public String uid(){return uid;}
     public String user(){return user;}
+    
     public boolean vegetarian() {
         return (vegetarian == 1);
     }
