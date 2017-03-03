33d7c35258fb65835544ff22586818ea0104fe9c
Jeff Koretke <jkoretke@ucsd.edu>
2017-03-03 01:25:47 -0800

    can upload pics from gallery

--- app/src/main/java/com/irs/main/controller/UploadPhoto.java
+++ app/src/main/java/com/irs/main/controller/UploadPhoto.java
@@ -1,7 +1,13 @@
 package com.irs.main.controller;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.irs.main.R;
 
@@ -11,9 +17,55 @@ import com.irs.main.R;
 
 public class UploadPhoto extends AppCompatActivity {
 
+    Button chosePhotoButton;
+    Bitmap pic;
+    ImageView selectedPic;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_photo);
+
+        selectedPic = (ImageView)findViewById(R.id.selected_pic);
+
+        chosePhotoButton = (Button)findViewById(R.id.chose_photo_button);
+        chosePhotoButton.setOnClickListener(new Button.OnClickListener(){
+           @Override
+            public void onClick(View v){
+                pickImage();
+           }
+        });
+    }
+
+    public void pickImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("scale", true);
+        intent.putExtra("outputX", 256);
+        intent.putExtra("outputY", 256);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data){
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == 1) {
+            final Bundle extras = data.getExtras();
+            if (extras != null) {
+                //Get image
+                pic = extras.getParcelable("data");
+                selectedPic.setImageBitmap(pic);
+            }
+        }
     }
 }
