70edef70431187fd5597cdebe08ca5bf9e7b639c
Jeff Koretke <jkoretke@ucsd.edu>
2017-03-06 12:23:57 -0800

    moved OpenMaps() to RestaurantList.java

--- app/src/main/java/com/irs/main/controller/RestaurantList.java
+++ app/src/main/java/com/irs/main/controller/RestaurantList.java
@@ -1,15 +1,18 @@
 package com.irs.main.controller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -55,7 +58,13 @@ public class RestaurantList extends ArrayAdapter<String> {
         DrawableCompat.setTint(progress, Color.BLACK);
 
         ImageView imageView = (ImageView)rowView.findViewById(R.id.img);
+        Button mapButton = (Button)view.findViewById(R.id.map_button);
+        mapButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
 
+            }
+        });
 
         //TODO this might block the UI thread.
         try {
@@ -70,6 +79,16 @@ public class RestaurantList extends ArrayAdapter<String> {
 
         return rowView;
 
+    }
+
+    private void openMaps(String coordinates, String label) {
+        String uriBegin = "geo:" + coordinates;
+        String query = coordinates + "(" + label + ")";
+        String encodedQuery = Uri.encode(query);
+        String uriString = uriBegin + "?q=" + encodedQuery + "&z=16";
+        Uri uri = Uri.parse(uriString);
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+        getContext().startActivity(intent);
     }
 
 }
