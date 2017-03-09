b2408d12912bb0195af929876011f4edadbffb3e
Jun Lee <jy45lee@gmail.com>
2017-03-09 08:50:00 -0800

    Update UploadPhotoController.java

--- app/src/main/java/com/irs/main/controller/UploadPhotoController.java
+++ app/src/main/java/com/irs/main/controller/UploadPhotoController.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,8 +15,14 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.irs.main.DietType;
 import com.irs.main.R;
+import com.irs.main.model.UserModel;
 import com.irs.server.ServerApi;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jeff on 3/3/17.
@@ -38,77 +45,50 @@ public class UploadPhotoController extends FragmentActivity {
 
     //TODO also change switch statement below for cultureSpinner.OnItemSelected
 
-    private static final String[] culturePaths = {"American (new)", "American (traditional)", "Chinese", "Cuban",
-            "Indian", "Italian", "Japanese", "Korean", "Mexican", "Russian", "Thai"};
+    private static final String[] culturePaths = {"American (new)","American (traditional)", "Chinese", "Cuban",
+            "Indian", "Italian","Japanese", "Korean","Mexican","Russian" ,"Thai" };
     private static final String[] dietPaths = {"None", "Vegetarian", "Vegan",
-            "Kosher", "Gluten Free"};
+        "Kosher", "Gluten Free"};
+    Map<String, DietType> dietMap = new HashMap<String, DietType>(){{
+        put("Gluten Free", DietType.GlutenFree);
+        put("Kosher", DietType.Kosher);
+        put("Vegan", DietType.Vegan);
+        put("Vegetarian", DietType.Vegetarian);
+        put("None", DietType.None);
+    }};
     private static final String[] categoryPaths = {"food", "desert", "fruit", "spicy"};
     private String picName = "";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_photo);
 
-        foodName = (EditText) findViewById(R.id.name_txt);
-        selectedPic = (ImageView) findViewById(R.id.selected_pic);
+        foodName = (EditText)findViewById(R.id.name_txt);
+        selectedPic = (ImageView)findViewById(R.id.selected_pic);
 
-        cultureSpinner = (Spinner) findViewById(R.id.culture_spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(UploadPhotoController.this,
+        cultureSpinner = (Spinner)findViewById(R.id.culture_spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(UploadPhoto.this,
                 android.R.layout.simple_spinner_item, culturePaths);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cultureSpinner.setAdapter(adapter);
 
-        dietSpinner = (Spinner) findViewById(R.id.dietary_spinner);
-        ArrayAdapter<String> dietAdapter = new ArrayAdapter<String>(UploadPhotoController.this,
+        dietSpinner = (Spinner)findViewById(R.id.dietary_spinner);
+        final ArrayAdapter<String>dietAdapter = new ArrayAdapter<String>(UploadPhoto.this,
                 android.R.layout.simple_spinner_item, dietPaths);
         dietSpinner.setAdapter(dietAdapter);
 
-        categorySpinner = (Spinner) findViewById(R.id.category_spinner);
-        ArrayAdapter<String> categoryAdapter = new
-                ArrayAdapter<String>(UploadPhotoController.this, android.R.layout.simple_spinner_item,
+        categorySpinner = (Spinner)findViewById(R.id.category_spinner);
+        final ArrayAdapter<String> categoryAdapter = new
+                ArrayAdapter<String>(UploadPhoto.this, android.R.layout.simple_spinner_item,
                 categoryPaths);
         categorySpinner.setAdapter(categoryAdapter);
 
         cultureSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0: //American (new)
-                        culture = culturePaths[0];
-                        break;
-                    case 1: //American (traditional)
-                        culture = culturePaths[1];
-                        break;
-                    case 2: //chinese
-                        culture = culturePaths[2];
-                        break;
-                    case 3: //Cuban
-                        culture = culturePaths[3];
-                        break;
-                    case 4: //Indian
-                        culture = culturePaths[4];
-                        break;
-                    case 5: //Italian
-                        culture = culturePaths[5];
-                        break;
-                    case 6: //Japanese
-                        culture = culturePaths[6];
-                        break;
-                    case 7: //Korean
-                        culture = culturePaths[7];
-                        break;
-                    case 8: //Mexican
-                        culture = culturePaths[8];
-                        break;
-                    case 9: //Russian
-                        culture = culturePaths[9];
-                        break;
-                    case 10: //Thai
-                        culture = culturePaths[10];
-                        break;
-                }
+                culture = culturePaths[position];
             }
 
             @Override
@@ -120,26 +100,7 @@ public class UploadPhotoController extends FragmentActivity {
         dietSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0://None
-                        diet = dietPaths[0];
-                        break;
-                    case 1://Vegetarian
-                        diet = dietPaths[1];
-                        break;
-                    case 2://Vegan
-                        diet = dietPaths[2];
-                        break;
-                    case 3://Kosher
-                        diet = dietPaths[3];
-                        break;
-                    case 4:
-                        diet = dietPaths[4];
-                        break;
-                    default:
-                        diet = "none";
-                        break;
-                }
+                diet = dietPaths[position];
             }
 
             @Override
@@ -151,26 +112,7 @@ public class UploadPhotoController extends FragmentActivity {
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //{"food", "desert", "fruit", "spicey"};
-                switch (position) {
-                    case 0://food
-                        category = categoryPaths[0];
-                        break;
-                    case 1: //desert
-                        category = categoryPaths[1];
-                        break;
-                    case 2://fruit
-                        category = categoryPaths[2];
-                        break;
-                    case 3://spicey
-                        category = categoryPaths[3];
-                        break;
-                    default://food
-                        category = categoryPaths[0];
-                        break;
-
-
-                }
+                category = categoryPaths[position];
             }
 
             @Override
@@ -179,53 +121,54 @@ public class UploadPhotoController extends FragmentActivity {
             }
         });
 
-        chosePhotoButton = (Button) findViewById(R.id.chose_photo_button);
-        chosePhotoButton.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        chosePhotoButton = (Button)findViewById(R.id.chose_photo_button);
+        chosePhotoButton.setOnClickListener(new Button.OnClickListener(){
+           @Override
+            public void onClick(View v){
                 pickImage();
-            }
+           }
         });
-        saveButton = (Button) findViewById(R.id.save_photo_button);
-        saveButton.setOnClickListener(new Button.OnClickListener() {
+        saveButton = (Button)findViewById(R.id.save_photo_button);
+        saveButton.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 try {
                     name = foodName.getText().toString();
                     System.out.println("Culture: " + culture + "\nDiet: " + diet +
                             "\ncategory: " + category + "\nname: " + name + "\npicName: " + picName);
-                    Toast.makeText(UploadPhotoController.this, "photo submitted!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(UploadPhoto.this, "photo submitted!", Toast.LENGTH_SHORT).show();
 
                     // upload image
-                    ServerApi.getInstance().uploadFood(pic, picName + ".jpg", name, culture, category, "220d763d2aa724c6a59d5649138a0211", 0, 0, 0, 0);
+                    UserModel.getInstance().uploadPhoto(pic, picName + ".jpg", name, culture, category, dietMap.get(diet));
 
                     finish();
-                } catch (Exception ex) {
+                }
+                catch (Exception ex){
                     System.out.println("You will be assimilated. Resistance is futile.");
                     ex.printStackTrace();
                 }
             }
         });
 
-        cancelButton = (Button) findViewById(R.id.cancel_photo_button);
-        cancelButton.setOnClickListener(new Button.OnClickListener() {
+        cancelButton = (Button)findViewById(R.id.cancel_photo_button);
+        cancelButton.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 finish();
             }
 
         });
-        cameraButton = (Button) findViewById(R.id.camera_button);
-        cameraButton.setOnClickListener(new Button.OnClickListener() {
+        cameraButton = (Button)findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Intent Intent3 = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+            public void onClick(View v){
+                Intent Intent3=new   Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
                 startActivity(Intent3);
             }
         });
     }
 
-    public void pickImage() {
+    public void pickImage(){
         Intent intent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         //TODO change these to crop and save as vertical phone pic
@@ -243,7 +186,7 @@ public class UploadPhotoController extends FragmentActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
+                                    Intent data){
         if (resultCode != RESULT_OK) {
             return;
         }
@@ -254,17 +197,14 @@ public class UploadPhotoController extends FragmentActivity {
                 for (String key : extras.keySet()) {
                     Object value = extras.get(key);
                     System.out.println("Key: " + key);
-                    if (value != null) {
-                        System.out.println(" value: " + value.toString() + " class: " + value.getClass().getName());
-                    }
-                    ;
+                    if (value != null) { System.out.println(" value: " + value.toString() + " class: " + value.getClass().getName());};
                 }
             }
             if (extras != null) {
                 //Get image
                 pic = extras.getParcelable("data");
                 String uri = extras.getString("src_uri");
-                String[] parts = uri.split("/");
+                String [] parts = uri.split("/");
                 picName = parts[parts.length - 1];
                 selectedPic.setImageBitmap(pic);
             }
