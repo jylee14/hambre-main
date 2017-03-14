cfa75088ae7706447345c385a2fc996aedf40023
Jun Lee <jy45lee@gmail.com>
2017-03-13 19:46:37 -0700

    separate login user from guests (dietary preferences)

--- app/src/main/java/com/irs/main/controller/DietRestrictionController.java
+++ app/src/main/java/com/irs/main/controller/DietRestrictionController.java
@@ -51,15 +51,11 @@ public class DietRestrictionController extends FragmentActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //if not guest mode
-                //push update to server);
-                //database.getUser(username)
-                //username.updateDiet(newPref, prefIndex);
-                UserModel.getInstance().saveToDatabaseAsync();
-                Toast.makeText(DietRestrictionController.this, "Changes saved!", Toast.LENGTH_SHORT).show();
+                if(!LandingController.isGuest) {
+                    UserModel.getInstance().saveToDatabaseAsync();
+                    Toast.makeText(DietRestrictionController.this, "Changes saved!", Toast.LENGTH_SHORT).show();
+                }
                 returnToPrev();
-                //else
-                //propagate the data throughout the app via preferenceID
             }
         });
     }
@@ -85,7 +81,8 @@ public class DietRestrictionController extends FragmentActivity {
                         dietTemp = DietType.GlutenFree;
                         break;
                 }
-                user.setDietType(dietTemp);
+                if(!LandingController.isGuest)
+                    user.setDietType(dietTemp);
             }
         });
     }
